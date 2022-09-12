e85a1a1dfb4b2e99cee0ed1160bb973ca10a1350
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-09-12 16:17:03 -0400

    Add office unit tests in integration project

--- tests/IntegrationTests/Offices/GetActiveStaffMembersList.cs
+++ tests/IntegrationTests/Offices/GetActiveStaffMembersList.cs
@@ -1,6 +1,43 @@
+using GaEpd.Library.Domain.Repositories;
+using MyAppRoot.Domain.Entities;
+using MyAppRoot.Domain.Offices;
+using MyAppRoot.TestData.Offices;
+
 namespace IntegrationTests.Offices;
 
 public class GetActiveStaffMembersList
 {
-    // TODO: Add unit tests
+    private IOfficeRepository _repository = default!;
+
+    [SetUp]
+    public void SetUp() => _repository = RepositoryHelper.CreateRepositoryHelper().GetOfficeRepository();
+
+    [TearDown]
+    public void TearDown() => _repository.Dispose();
+
+    [Test]
+    public async Task WhenStaffExist_ReturnsList()
+    {
+        // First active office was seeded with active staff.
+        var item = OfficeData.GetOffices.First(e => e.Active);
+        var result = await _repository.GetActiveStaffMembersListAsync(item.Id);
+        result.Should().BeEquivalentTo(item.StaffMembers);
+    }
+
+    [Test]
+    public async Task WhenStaffDoNotExist_ReturnsEmptyList()
+    {
+        var item = OfficeData.GetOffices.Last(e => e.Active);
+        var result = await _repository.GetActiveStaffMembersListAsync(item.Id);
+        result.Should().BeEmpty();
+    }
+
+    [Test]
+    public async Task WhenOfficeDoesNotExist_Throws()
+    {
+        var id = Guid.Empty;
+        var action = async () => await _repository.GetActiveStaffMembersListAsync(id);
+        (await action.Should().ThrowAsync<EntityNotFoundException>())
+            .WithMessage($"Entity not found. Entity type: {typeof(Office).FullName}, id: {id}");
+    }
 }
