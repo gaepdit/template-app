4c7114ae6a30fe9a84891ffcd477082bedf0d980
Pat <pat.tran@dnr.ga.gov>
2022-10-17 11:37:56 -0400

    add settings to ignore cert errors

--- src/WebApp/lighthouserc.js
+++ src/WebApp/lighthouserc.js
@@ -3,6 +3,9 @@ module.exports = {
       collect: {
         url: ['https://localhost:7229/'],
         startServerCommand: 'dotnet run --no-build',
+        settings: {
+            "chromeFlags": "--ignore-certificate-errors --no-sandbox"
+        },
       },
       upload: {
         target: 'temporary-public-storage',
