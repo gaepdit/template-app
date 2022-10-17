11d74a387c4472f687a42ebd7f73b959500bc43b
Pat <pat.tran@dnr.ga.gov>
2022-10-17 12:40:39 -0400

    change to custom assertions

--- src/WebApp/lighthouserc.js
+++ src/WebApp/lighthouserc.js
@@ -9,7 +9,12 @@ module.exports = {
         },
       },
       assert: {
-        preset: 'lighthouse:no-pwa',
+        assertions: {
+            "categories:performance": ["error", {"minScore": 0.9}],
+            "categories:accessibility": ["error", {"minScore": 0.9}],
+            "categories:best-practices": ["error", {"minScore": 0.9}],
+            "categories:seo": ["error", {"minScore": 0.8}],
+        },
       },
       upload: {
         target: 'temporary-public-storage',
