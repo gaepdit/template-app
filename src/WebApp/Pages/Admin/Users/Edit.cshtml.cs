75d8ecfff0bdfc7421c04bdd72b207056731a8da
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-01-06 09:28:36 -0500

    Handle error status codes with custom error page

--- src/WebApp/Pages/Admin/Users/Edit.cshtml.cs
+++ src/WebApp/Pages/Admin/Users/Edit.cshtml.cs
@@ -20,7 +20,8 @@ public class EditModel : PageModel
     private readonly IOfficeAppService _officeService;
     private readonly IValidator<StaffUpdateDto> _validator;
 
-    public EditModel(IStaffAppService staffService, IOfficeAppService officeService, IValidator<StaffUpdateDto> validator)
+    public EditModel(IStaffAppService staffService, IOfficeAppService officeService,
+        IValidator<StaffUpdateDto> validator)
     {
         _staffService = staffService;
         _officeService = officeService;
@@ -36,9 +37,9 @@ public class EditModel : PageModel
 
     public async Task<IActionResult> OnGetAsync(Guid? id)
     {
-        if (id == null) return NotFound();
+        if (id == null) return RedirectToPage("Index");
         var staff = await _staffService.FindAsync(id.Value);
-        if (staff == null) return NotFound("ID not found.");
+        if (staff == null) return NotFound();
 
         DisplayStaff = staff;
         UpdateStaff = DisplayStaff.AsUpdateDto();
