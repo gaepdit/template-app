10c59125267438a1b63c0150e62e472d730b9413
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-01-10 16:05:09 -0500

    Auto-include User Office data in EF
    Includes can't be modified when using the Microsoft Identity UserManager, so the only way to get Office data is to auto-include it.

--- src/Infrastructure/Contexts/AppDbContext.cs
+++ src/Infrastructure/Contexts/AppDbContext.cs
@@ -12,6 +12,14 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
     // Add domain entities here.
     public DbSet<Office> Offices => Set<Office>();
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // See https://learn.microsoft.com/en-us/ef/core/querying/related-data/eager#model-configuration-for-auto-including-navigations
+        builder.Entity<ApplicationUser>().Navigation(e => e.Office).AutoInclude();
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
