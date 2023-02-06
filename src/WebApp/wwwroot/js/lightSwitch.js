661c0f50b332d2694fbe9a61fd4f83cf185f81e3
Pat <pat.tran@dnr.ga.gov>
2023-02-06 16:34:49 -0500

    switched from toggler to dropdown

--- src/WebApp/wwwroot/js/lightSwitch.js
+++ src/WebApp/wwwroot/js/lightSwitch.js
@@ -1,106 +1,61 @@
-(function () {
-  let lightSwitch = document.getElementById('lightSwitch');
-  if (!lightSwitch) {
-    return;
-  }
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2022 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
 
-  /**
-   * @function darkMode
-   * @summary: changes the theme to 'dark mode' and save settings to local storage.
-   */
-  function darkMode() {
-    document.documentElement.setAttribute('data-bs-theme', 'dark')
-    // set light switch input to dark
-    if (lightSwitch.checked) {
-      lightSwitch.checked = false;
-    }
-    localStorage.setItem('theme', 'dark');
-  }
+(() => {
+    'use strict'
 
-  /**
-   * @function lightMode
-   * @summary: changes the theme to 'light mode' and save settings to local storage.
-   */
-  function lightMode() {
-    document.documentElement.setAttribute('data-bs-theme', 'light')
-    // set light switch input to light
-    if (!lightSwitch.checked) {
-      lightSwitch.checked = true;
-    }
-    localStorage.setItem('theme', 'light');
-  }
-
-  /**
-   * @function setTheme
-   * @summary: calling @darkMode or @lightMode depending on the checked state.
-   */
-  function setTheme() {
-    if (!lightSwitch.checked) {
-      darkMode();
-    } else {
-      lightMode();
-    }
-  }
-
-  /**
-   * @function getTheme
-   * @summary: get system default theme by media query
-   */
-  function getTheme() {
     const storedTheme = localStorage.getItem('theme')
-    if (storedTheme) {
-      return storedTheme
-    }
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-  }
 
-  /**
-   * @function showActiveTheme
-   * @param theme light or dark mode
-   */
-  function showActiveTheme(theme) {
-    const activeThemeIcon = document.querySelector('.theme-icon-active use')
-    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-    const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
-
-    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-      element.classList.remove('active')
-    })
-
-    btnToActive.classList.add('active')
-    activeThemeIcon.setAttribute('href', svgOfActiveBtn)
-  }
-
-  /**
-   * @summary A function called in the beginning. Add event listener and determine which mode to use.
-   */
-  function setup() {
-    let settings = localStorage.getItem('theme');
-    if (settings == null) {
-      settings = getTheme();
-    }
-    if (settings === 'light') {
-      lightSwitch.checked = true;
+    const getPreferredTheme = () => {
+        if (storedTheme) {
+            return storedTheme
+        }
+        return 'auto'
     }
 
-    lightSwitch.addEventListener('change', setTheme);
-    
-    // Change the default theme on launch of page
-    window.addEventListener('DOMContentLoaded', () => {
-      setTheme()
-
-      document.querySelectorAll('[data-bs-theme-value]')
-        .forEach(toggle => {
-          toggle.addEventListener('click', () => {
-            const theme = toggle.getAttribute('data-bs-theme-value')
-            localStorage.setItem('theme', theme)
+    const setTheme = function (theme) {
+        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        } else {
             document.documentElement.setAttribute('data-bs-theme', theme)
-            showActiveTheme(theme)
-          })
-        })
-    })
-    setTheme();
-  }
+        }
+    }
 
-  setup();
-})();
+    setTheme(getPreferredTheme())
+
+    const showActiveTheme = theme => {
+        const activeThemeIcon = document.querySelector('.theme-icon-active use')
+        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+        const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+
+        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+            element.classList.remove('active')
+        })
+
+        btnToActive.classList.add('active')
+        activeThemeIcon.setAttribute('href', svgOfActiveBtn)
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        if (storedTheme !== 'light' || storedTheme !== 'dark') {
+            setTheme(getPreferredTheme())
+        }
+    })
+
+    window.addEventListener('DOMContentLoaded', () => {
+        showActiveTheme(getPreferredTheme())
+
+        document.querySelectorAll('[data-bs-theme-value]')
+            .forEach(toggle => {
+                toggle.addEventListener('click', () => {
+                    const theme = toggle.getAttribute('data-bs-theme-value')
+                    localStorage.setItem('theme', theme)
+                    setTheme(theme)
+                    showActiveTheme(theme)
+                })
+            })
+    })
+})()
