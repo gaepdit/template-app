18516ebe60b860bd396cd1489c25a260aaec3fcc
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-05-02 15:51:30 -0400

    Reset static data before each local repository test

---
+++ tests/LocalRepositoryTests/RepositoryHelper.cs
@@ -0,0 +1,27 @@
+﻿using MyAppRoot.LocalRepository.Identity;
+using MyAppRoot.LocalRepository.Repositories;
+using MyAppRoot.TestData;
+using MyAppRoot.TestData.Identity;
+
+namespace LocalRepositoryTests;
+
+public static class RepositoryHelper
+{
+    public static LocalOfficeRepository GetOfficeRepository()
+    {
+        ClearAllStaticData();
+        return new LocalOfficeRepository();
+    }
+
+    public static LocalUserStore GetLocalUserStore()
+    {
+        ClearAllStaticData();
+        return new LocalUserStore();
+    }
+
+    private static void ClearAllStaticData()
+    {
+        OfficeData.ClearData();
+        UserData.ClearData();
+    }
+}
