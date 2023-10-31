b0b3cc79cb256ea9caa8144a9e802b59089d8319
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-10-31 15:54:12 -0400

    Use generated regex

--- src/Domain/GlobalUsings.cs
+++ src/Domain/GlobalUsings.cs
@@ -2,4 +2,3 @@
 global using GaEpd.AppLibrary.Domain.Repositories;
 global using JetBrains.Annotations;
 global using System.ComponentModel.DataAnnotations;
-global using System.ComponentModel.DataAnnotations.Schema;
