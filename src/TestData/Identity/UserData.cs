2db60c1c57b6f5cbe5e9f4a6a77985c98c7b7f46
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-11-21 09:45:03 -0500

    Change how test user data is seeded

--- src/TestData/Identity/UserData.cs
+++ src/TestData/Identity/UserData.cs
@@ -5,7 +5,7 @@ namespace MyApp.TestData.Identity;
 
 internal static partial class UserData
 {
-    private static IEnumerable<ApplicationUser> UserSeedItems => new List<ApplicationUser>
+    private static IEnumerable<ApplicationUser> UserSeedItems { get; } = new List<ApplicationUser>
     {
         new()
         {
