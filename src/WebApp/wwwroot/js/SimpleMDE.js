c58c9b643f513bda0b5de8f4b68623eee11f413d
Caleb Nguyen <caleb.nguyen@dnr.ga.gov>
2024-06-11 14:09:00 -0400

    Created SimpleMDE.js file. Got rich text editor to appear using custom <textarea>

---
+++ src/WebApp/wwwroot/js/SimpleMDE.js
@@ -0,0 +1,4 @@
+﻿var simplemde = new SimpleMDE({
+    toolbar: ["bold", "italic", "strikethrough", "|", "heading", "quote", "|", "unordered-list", "ordered-list", "|", "preview", "fullscreen", "guide"],
+    element: document.getElementById("MyID")
+});
