e1e95d81b0e3fe87e7c377dd3a2e06af89e6ff1f
Caleb Nguyen <caleb.nguyen@dnr.ga.gov>
2024-07-12 14:29:56 -0400

    Changes to toolbar

--- src/WebApp/wwwroot/js/textarea-to-MDE.js
+++ src/WebApp/wwwroot/js/textarea-to-MDE.js
@@ -2,5 +2,23 @@ const textarealist = document.querySelectorAll("textarea.EasyMDE");
 textarealist.forEach((area) => {
   const easyMDEE = new EasyMDE({
     element: area,
+    placeholder: "Type here...",
+    toolbar: [
+      "bold",
+      "italic",
+      "strikethrough",
+      "|",
+      "heading-1",
+      "heading-2",
+      "heading-3",
+      "|",
+      "quote",
+      "unordered-list",
+      "ordered-list",
+      "|",
+      "preview",
+      "fullscreen",
+      "guide",
+    ],
   });
 });
