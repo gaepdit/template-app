821386ec9357fcc9cb4fa0eb72c6088c044f52df
Caleb Nguyen <caleb.nguyen@dnr.ga.gov>
2024-07-12 10:15:56 -0400

    Added textarea-to-MDE.js

---
+++ src/WebApp/wwwroot/js/textarea-to-MDE.js
@@ -0,0 +1,6 @@
+const easyMDE = new EasyMDE({
+  element: document.getElementById("my-text-area"),
+});
+const easyMDEE = new EasyMDE({
+  element: document.querySelectorAll("textarea.EasyMDE"),
+});
