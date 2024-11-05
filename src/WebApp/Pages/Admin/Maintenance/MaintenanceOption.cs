cfeaaf40fb0c6f61eb681ad6bc1f85445a14b9e4
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-11-05 15:35:51 -0500

    Implement consistent SVG icons
    Also adds a warning message when editing a maintenance item.

--- src/WebApp/Pages/Admin/Maintenance/MaintenanceOption.cs
+++ src/WebApp/Pages/Admin/Maintenance/MaintenanceOption.cs
@@ -4,12 +4,13 @@ public class MaintenanceOption
 {
     public string SingularName { get; private init; } = string.Empty;
     public string PluralName { get; private init; } = string.Empty;
+    public bool StartsWithVowelSound { get; private init; }
 
     private MaintenanceOption() { }
 
     public static MaintenanceOption EntryType { get; } =
-        new() { SingularName = "Work Entry Type", PluralName = "Work Entry Types" };
+        new() { SingularName = "Work Entry Type", PluralName = "Work Entry Types", StartsWithVowelSound = false };
 
     public static MaintenanceOption Office { get; } =
-        new() { SingularName = "Office", PluralName = "Offices" };
+        new() { SingularName = "Office", PluralName = "Offices", StartsWithVowelSound = true };
 }
