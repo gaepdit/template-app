69a3210f9429515e3f2cb7b8648e88e9a2bedd22
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-01-09 16:31:49 -0500

    Output DB setup commands during development

--- tests/EfRepositoryTests/RepositoryHelper.cs
+++ tests/EfRepositoryTests/RepositoryHelper.cs
@@ -1,5 +1,6 @@
 using GaEpd.AppLibrary.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using MyApp.Domain.Entities.EntryTypes;
 using MyApp.Domain.Entities.Offices;
 using MyApp.Domain.Entities.WorkEntries;
@@ -26,7 +27,7 @@ namespace EfRepositoryTests;
 /// </summary>
 public sealed class RepositoryHelper : IDisposable, IAsyncDisposable
 {
-    private AppDbContext Context { get; set; } = default!;
+    private AppDbContext Context { get; set; } = null!;
 
     private readonly DbContextOptions<AppDbContext> _options;
     private readonly AppDbContext _context;
@@ -36,7 +37,8 @@ public sealed class RepositoryHelper : IDisposable, IAsyncDisposable
     /// </summary>
     private RepositoryHelper()
     {
-        _options = SqliteInMemory.CreateOptions<AppDbContext>();
+        _options = SqliteInMemory.CreateOptions<AppDbContext>(builder =>
+            builder.LogTo(Console.WriteLine, events: [RelationalEventId.CommandExecuted]));
         _context = new AppDbContext(_options);
         _context.Database.EnsureCreated();
     }
@@ -49,7 +51,8 @@ public sealed class RepositoryHelper : IDisposable, IAsyncDisposable
     private RepositoryHelper(object callingClass, string callingMember)
     {
         _options = callingClass.CreateUniqueMethodOptions<AppDbContext>(callingMember: callingMember,
-            builder: opts => opts.UseSqlServer());
+            builder: builder => builder.UseSqlServer()
+                .LogTo(Console.WriteLine, events: [RelationalEventId.CommandExecuted]));
         _context = new AppDbContext(_options);
         _context.Database.EnsureClean();
     }
