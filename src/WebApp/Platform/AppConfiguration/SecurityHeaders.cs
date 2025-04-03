d4e5aefb3ca9fd14352a60e99667566da74a74d4
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-04-03 10:03:00 -0400

    Configure HTTP security headers
    - Update the SecurityHeaders package
    - Configure the security header and minimal CSP policies (initially set to report only)
    - Add CSP tag helpers

--- src/WebApp/Platform/AppConfiguration/SecurityHeaders.cs
+++ src/WebApp/Platform/AppConfiguration/SecurityHeaders.cs
@@ -4,17 +4,32 @@ namespace MyApp.WebApp.Platform.AppConfiguration;
 
 internal static class SecurityHeaders
 {
+    private static readonly string ReportUri =
+        $"https://report-to-api.raygun.com/reports?apikey={AppSettings.RaygunSettings.ApiKey}";
+
     internal static void AddSecurityHeaderPolicies(this HeaderPolicyCollection policies)
     {
-        policies
-            .AddFrameOptionsDeny()
-            .AddXssProtectionBlock()
-            .AddContentTypeOptionsNoSniff()
-            .AddReferrerPolicyStrictOriginWhenCrossOrigin()
-            .RemoveServerHeader();
+        policies.AddFrameOptionsDeny();
+        policies.AddContentTypeOptionsNoSniff();
+        policies.AddReferrerPolicyStrictOriginWhenCrossOrigin();
+        policies.RemoveServerHeader();
+        policies.AddContentSecurityPolicyReportOnly(builder => builder.CspBuilder());
+        policies.AddCrossOriginOpenerPolicy(builder => builder.SameOrigin());
+        policies.AddCrossOriginEmbedderPolicy(builder => builder.Credentialless());
+        policies.AddCrossOriginResourcePolicy(builder => builder.SameSite());
 
-        if (!string.IsNullOrEmpty(AppSettings.RaygunSettings.ApiKey))
-            policies.AddReportingEndpoints(builder => builder.AddEndpoint("csp-endpoint",
-                $"https://report-to-api.raygun.com/reports-csp?apikey={AppSettings.RaygunSettings.ApiKey}"));
+        if (string.IsNullOrEmpty(AppSettings.RaygunSettings.ApiKey)) return;
+        policies.AddReportingEndpoints(builder => builder.AddEndpoint("csp-endpoint", ReportUri));
+    }
+
+    private static void CspBuilder(this CspBuilder builder)
+    {
+        builder.AddObjectSrc().None();
+        builder.AddFormAction().Self();
+        builder.AddFrameAncestors().None();
+
+        if (string.IsNullOrEmpty(AppSettings.RaygunSettings.ApiKey)) return;
+        builder.AddReportUri().To(ReportUri);
+        builder.AddReportTo("csp-endpoint");
     }
 }
